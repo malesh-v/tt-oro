50b673de8361f7c415c92f390020c0155fefa405
Malesh Vadim <vadim.malesh01@gmail.com>
2025-11-06 10:32:03 +0100

    register head bundle

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    App\ChainCommandBundle\ChainCommandBundle::class => ['all' => true],
 ];
